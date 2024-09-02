b160750b9b4ff4a92599014f9e6bafbf7895e880
adars124 <lonleyau@gmail.com>
2024-09-02 14:09:15 +0545

    basic modifications done

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React from "react";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "./context/AuthContext";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "./theme/theme";
-
 import Routes from "./routes";
+
+const queryClient = new QueryClient();
 
 const App: React.FC = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Routes />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes />
+        </ThemeProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 };
 
