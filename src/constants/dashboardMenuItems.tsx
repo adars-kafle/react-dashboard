efa8f13a4ebb32c74c6623241bd38811879cefa7
adars124 <lonleyau@gmail.com>
2024-09-02 14:00:12 +0545

    exported dashboard menu items and error messages

---
+++ src/constants/dashboardMenuItems.tsx
@@ -0,0 +1,7 @@
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+
+export const menuItems = [
+  { text: "Dashboard", icon: <DashboardIcon />, path: "/" },
+  { text: "Suppliers", icon: <LocalShippingIcon />, path: "/suppliers" },
+];
