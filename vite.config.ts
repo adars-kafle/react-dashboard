c758cf30eec2e6d2f2da55a7d1768c88336f296b
adars124 <lonleyau@gmail.com>
2024-09-10 13:50:57 +0545

    feat: tried adding import alias (unsuccessful)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   optimizeDeps: {
     include: ["@mui/x-date-pickers/DatePicker", "@emotion/styled"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
