641ca3c42f2fad4da2ba9565643b3f6cb6794d23
adars124 <lonleyau@gmail.com>
2024-09-10 13:45:19 +0545

    refactor: use lazy load utility

--- src/components/LazyComponents/index.tsx
+++ src/components/LazyComponents/index.tsx
@@ -1,10 +1,8 @@
 import { lazyLoad } from "../../utils/lazyLoad";
 
-export const LoginPage = lazyLoad(
-  () => import("../../pages/Authentication/LoginPage")
-);
+export const LoginPage = lazyLoad(() => import("../Authentication/LoginPage"));
 export const SignupPage = lazyLoad(
-  () => import("../../pages/Authentication/SignupPage")
+  () => import("../Authentication/SignupPage")
 );
 export const DashboardPage = lazyLoad(() => import("../../pages/Dashboard"));
 export const SuppliersPage = lazyLoad(() => import("../../pages/Suppliers"));
